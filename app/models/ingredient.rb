64cb56ac2291fb1c1053f35118a319b9b6d6a250
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2024-12-17 12:31:58 +0530

    changes related to ui

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -7,7 +7,7 @@ class Ingredient < ApplicationRecord
 
   def self.search(search)
     if search
-      left_joins(:products).where("ingredients.name ILIKE ? or products.name ILIKE ?", "%#{search}%", "%#{search}%")
+      left_joins(:products).where("ingredients.name ILIKE ? or products.name ILIKE ? or products.product_code ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%").distinct
     else
       scoped
     end
