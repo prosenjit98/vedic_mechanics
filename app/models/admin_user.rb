efe26d2b7a1afd48c79a4138c086ccf48bfb508a
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-04-04 17:43:53 +0530

    contact information updated

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,6 +6,6 @@ class AdminUser < ApplicationRecord
   has_many :replies
 
 
-  ADMIN_EMAILS = ["pmikuk@gmail.com", "looserpro2024@gamil.com"]
+  ADMIN_EMAILS = ["pmikuk@gmail.com", "looserpro2024@gamil.com", "preciagrotech@gmail.com", "nutrivedic@gmail.com"]
 
 end
