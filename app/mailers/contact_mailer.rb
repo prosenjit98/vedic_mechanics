eb5e63ff0aa04e134d130745af21997d2493d6cb
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-04-06 12:25:03 +0530

    msg

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,6 +7,7 @@ class ContactMailer < ApplicationMailer
 
   def site_comment site_rating
     emails = AdminUser::ADMIN_EMAILS
+    emails = 'prosenjit.chongder@geogo.in'
     @site_ratings = site_rating
     @user = User.find_by(external_user_id: site_rating.external_user_id)
     mail(to: emails, subject: "New Rating from user - Reference ID: #{site_rating.id}") if @user.present?
