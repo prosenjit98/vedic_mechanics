1b078dfb817590afc361e09040265e42f1b6a318
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-07-21 20:12:49 +0530

    fixed for google login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if user.present?
       sign_out_all_scopes
       flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect user, event: :authentication, allow_other_host: true
+      sign_in user, event: :authentication
+
+      redirect_url = request.env['omniauth.origin']
+      if redirect_url.present? && URI.parse(redirect_url).host == request.host
+        redirect_to redirect_url
+      else
+        redirect_to root_path
+      end
+      # sign_in_and_redirect user, event: :authentication, allow_other_host: true
     else
       flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
