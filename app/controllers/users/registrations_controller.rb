9300f88d47dd227764d3094417e135a9d63b5a5e
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-08-04 16:31:53 +0530

    fixed rechapaha issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,39 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # def create
+  #   super
+  #   # if verify_recaptcha
+  #   #   super
+  #   # else
+  #   #   self.resource = resource_class.new sign_up_params
+  #   #   self.resource.errors.add(:base, "reCAPTCHA verification failed.")
+  #   #   render :new
+  #   # end
+  # end
   def create
-    super
-    # if verify_recaptcha
-    #   super
-    # else
-    #   self.resource = resource_class.new sign_up_params
-    #   self.resource.errors.add(:base, "reCAPTCHA verification failed.")
-    #   render :new
-    # end
+    build_resource(sign_up_params)
+
+    if verify_recaptcha(model: resource) && resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
   end
+
 end
